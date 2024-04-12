064822ff26b45fceaeb25c649b4a5639bdbd753b
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2024-04-13 01:42:04 +0900

    feat: 移动端布局

--- docs/src/components/HomePage.vue
+++ docs/src/components/HomePage.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import TableOfContents from "./TableOfContents.vue";
-import ProfileCard from './ProfileCard.vue'
+import ProfileCard from "./ProfileCard.vue";
+import { useWindowSize } from "@vueuse/core";
+
+const { width } = useWindowSize();
 
 import { useData } from "vitepress";
 
@@ -14,9 +17,11 @@ if (theme.value.userProfile) {
 </script>
 
 <template>
-  <div class="home-page w-[100dvw] pl-[48px] pr-[48px] pt-[24px] flex flex-col items-center">
-    <div class="max-w-[1200px] flex gap-[48px] w-full">
-      <ProfileCard :props="props"/>
+  <div
+    class="w-[100dvw] pl-[48px] pr-[48px] pt-[24px] flex flex-col items-center"
+  >
+    <div class="home-page max-w-[1200px] flex gap-[48px] w-full">
+      <ProfileCard :props="props" v-if="width >= 425"/>
       <TableOfContents class="home-page__toc" />
     </div>
   </div>
@@ -25,7 +30,7 @@ if (theme.value.userProfile) {
 <style scoped lang="scss">
 @media (max-width: 475px) {
   .home-page {
-    flex-direction: row;
+    flex-direction: column;
   }
 }
 </style>
