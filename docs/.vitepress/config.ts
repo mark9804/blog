b11a88778b7956f7200c76fbb2c14c1292bf81d6
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2024-11-10 21:52:49 +0900

    feat: sitemap

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vitepress";
 import { HeadConfig, DefaultTheme } from "vitepress";
-import { BASE_URL } from "./constants";
+import { BASE_URL, DOMAIN } from "./constants";
 import { headConfig } from "./configs/headConfig";
 import { markdownConfig } from "./configs/markdownConfig";
 import { themeConfig } from "./configs/themeConfig";
@@ -16,6 +16,9 @@ export default defineConfig(
       title: "今天没有早睡",
       head: headConfig as HeadConfig[],
       description: "",
+      sitemap: {
+        hostname: DOMAIN + BASE_URL,
+      },
       lastUpdated: true,
       markdown: markdownConfig,
       themeConfig: themeConfig as DefaultTheme.Config,
