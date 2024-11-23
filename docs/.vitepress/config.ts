dfd96bee3a7f842a4814d36065ad5b4568d401c0
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2024-11-23 16:20:05 +0900

    fix: 侧边栏不能正确应用 `is-active`

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,23 +8,40 @@ import { viteConfig } from "./configs/viteConfig";
 import { sidebarConfig } from "./configs/sidebarConfig";
 import { withSidebar } from "vitepress-sidebar";
 
+// withSidebar 在设置了 `resolvePath` 后不会在 `link` 前添加 `/`，
+// 导致 VitePress 不能正确应用 `is-active` 类
+// 这里手动递归处理一下添加
+function processLinks(
+  items: DefaultTheme.SidebarItem[]
+): DefaultTheme.SidebarItem[] {
+  return items.map(item => {
+    if (item.items) {
+      item.items = processLinks(item.items);
+    }
+    if (item.link && !item.link.startsWith("/")) {
+      item.link = "/" + item.link;
+    }
+    return item;
+  });
+}
+
 // https://vitepress.dev/reference/site-config
-export default defineConfig(
-  withSidebar(
-    {
-      base: BASE_URL,
-      title: "今天没有早睡",
-      head: headConfig as HeadConfig[],
-      description: "",
-      lang: "zh-CN",
-      sitemap: {
-        hostname: DOMAIN + BASE_URL,
-      },
-      lastUpdated: true,
-      markdown: markdownConfig,
-      themeConfig: themeConfig as DefaultTheme.Config,
-      vite: viteConfig,
-    },
-    sidebarConfig
-  )
-);
+export default defineConfig({
+  base: BASE_URL,
+  title: "今天没有早睡",
+  head: headConfig as HeadConfig[],
+  description: "",
+  lang: "zh-CN",
+  sitemap: {
+    hostname: DOMAIN + BASE_URL,
+  },
+  lastUpdated: true,
+  markdown: markdownConfig,
+  themeConfig: {
+    ...(themeConfig as DefaultTheme.Config),
+    sidebar: processLinks(
+      withSidebar({}, sidebarConfig).themeConfig.sidebar || []
+    ),
+  },
+  vite: viteConfig,
+});
