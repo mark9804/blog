86d627b296f06400514987ba8cb927e1c3164492
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2024-11-23 20:27:43 +0900

    feat: 侧边栏展示头像

--- docs/.vitepress/configs/themeConfig.ts
+++ docs/.vitepress/configs/themeConfig.ts
@@ -4,6 +4,7 @@ import { tokenize } from "../utils/tokenizer";
 
 export const themeConfig = {
   userProfile: userProfile,
+  logo: "/" + userProfile.avatar,
   lastUpdated: {
     text: "最后更新于",
     formatOptions: {
@@ -28,8 +29,8 @@ export const themeConfig = {
 
   footer: {
     message:
-      "We improve our products and advertising by using Microsoft Clarity to see how you use our website. By using our site, you agree that we and Microsoft can collect and use this data.",
-    copyright: `© 2024-${new Date().getFullYear()} Mark Chen`,
+      "Powered by <a href='https://vitepress.dev' target='_blank'>VitePress</a> and <a href='https://github.com/mark9804/blog/tree/master/docs/.vitepress/theme/components/ElysiumUI' target='_blank'>Elysium UI</a><br>We improve our products and advertising by using Microsoft Clarity to see how you use our website. By using our site, you agree that we and Microsoft can collect and use this data.",
+    copyright: `© 2024-${new Date().getFullYear() === 2024 ? "now" : new Date().getFullYear()} Mark Chen`,
   },
 
   socialLinks: [{ icon: "github", link: "https://github.com/mark9804" }],
