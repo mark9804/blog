ab07065bc9aae7d620197289340597041761912a
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2024-11-28 21:19:34 +0900

    post: trackpad 4

--- docs/.vitepress/theme/components/ElysiumUI/_utils/stringUtils.ts
+++ docs/.vitepress/theme/components/ElysiumUI/_utils/stringUtils.ts
@@ -1,4 +1,5 @@
 export function formalizeString(str: string) {
-  const shouldDisposeChars = /[\`]/g;
+  const shouldDisposeChars = /`/g;
+  // @ts-ignore: 消不掉 replaceAll 错误，但我觉得我没有错
   return str.replaceAll(shouldDisposeChars, "");
 }
