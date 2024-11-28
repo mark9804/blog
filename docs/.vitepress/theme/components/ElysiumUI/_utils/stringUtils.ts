61356942e14fe282bf9dd2dbf2d7d3671472bfd8
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2024-11-29 02:13:03 +0900

    fix: 标题可能为空

--- docs/.vitepress/theme/components/ElysiumUI/_utils/stringUtils.ts
+++ docs/.vitepress/theme/components/ElysiumUI/_utils/stringUtils.ts
@@ -1,4 +1,5 @@
-export function formalizeString(str: string) {
+export function formalizeString(str: string | undefined) {
+  if (!str) return "";
   const shouldDisposeChars = /`/g;
   // @ts-ignore: 消不掉 replaceAll 错误，但我觉得我没有错
   return str.replaceAll(shouldDisposeChars, "");
