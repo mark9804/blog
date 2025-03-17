173280d8ebe747b7497b887b92fc30ac876dc99d
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2025-03-17 14:58:02 +0800

    chore: 更新导航链接

--- docs/.vitepress/configs/navConfig.ts
+++ docs/.vitepress/configs/navConfig.ts
@@ -1,15 +1,16 @@
 export const navConfig = [
   { text: "首页", link: "/" },
-  {
-    text: "碧蓝档案剧情站",
-    items: [
-      { text: "首页", link: "https://blue-archive.io" },
-      { text: "Bilibili", link: "https://space.bilibili.com/1413213021" },
-    ],
-  },
+  { text: "关于", link: "/about" },
   {
     text: "友链",
     items: [
+      {
+        text: "碧蓝档案剧情站",
+        items: [
+          { text: "首页", link: "https://blue-archive.io" },
+          { text: "Bilibili", link: "https://space.bilibili.com/1413213021" },
+        ],
+      },
       { text: "Arona Bot", link: "https://doc.arona.diyigemt.com/" },
       { text: "BA OST Index", link: "https://ba.cnfast.top/" },
       {
