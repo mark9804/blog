d11147df5570060f7ce8480f10163e98770ac1c2
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2025-04-20 22:38:05 +0800

    chore: make eslint great again

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,29 @@
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import vueParser from "vue-eslint-parser";
+import vuePlugin from "eslint-plugin-vue";
+
 export default [
   {
-    env: {
-      browser: true,
-      es2021: true,
+    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.browser,
+      },
     },
     rules: {
-      "vue/no-multiple-template-root": "off",
-      "vue/multi-word-component-names": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
       indent: ["error", 2, { SwitchCase: 1 }],
       "max-len": ["error", { code: 120 }],
       "linebreak-style": ["error", "unix"],
@@ -21,16 +38,50 @@ export default [
           functions: "ignore",
         },
       ],
-      "sort-exports/sort-exports": ["error", { sortDir: "asc" }],
-      "sort-imports": [
+    },
+  },
+  {
+    files: ["**/*.vue"],
+    plugins: {
+      vue: vuePlugin,
+    },
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+        extraFileExtensions: [".vue"],
+      },
+    },
+    rules: {
+      "vue/no-multiple-template-root": "off",
+      "vue/multi-word-component-names": "off",
+      "vue/block-order": [
         "error",
         {
-          ignoreCase: false,
-          ignoreDeclarationSort: true,
-          ignoreMemberSort: false,
-          memberSyntaxSortOrder: ["all", "single", "multiple", "none"],
+          order: ["script", "template", "style"],
+        },
+      ],
+      "vue/block-lang": [
+        "error",
+        {
+          script: { lang: "ts" },
+          style: { lang: "scss" },
         },
       ],
     },
+  },
+  {
+    ignores: [
+      "node_modules",
+      ".DS_Store",
+      "dist",
+      "dist-ssr",
+      "*.local",
+      "presets",
+      "*.d.ts",
+      "gstc.wasm.esm.min.js",
+    ],
   },
 ];
