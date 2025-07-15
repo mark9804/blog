d1574e1a4076d23fbffeb29e7a83da9cea402fbc
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2025-07-15 22:45:19 +0900

    feat: 空状态组件

---
+++ docs/.vitepress/theme/components/ElysiumUI/ElyEmptyState.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import ElyIconEmpty from "./ElyIcon/ElyIconEmpty.vue";
+</script>
+
+<template>
+  <div class="ely-empty-state flex flex-col items-center justify-center gap-4">
+    <ElyIconEmpty size="100" color="var(--color-accent-text-tertiary)" />
+    <p class="text-sm text-tertiary ellipsis-followed">
+      你似乎来到了篇章的尾声
+    </p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.ellipsis-followed::after {
+  content: "……";
+  display: inline-block;
+  overflow: hidden;
+  vertical-align: bottom;
+}
+</style>
