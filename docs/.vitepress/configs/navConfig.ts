e69bba7c161070916e5925b050648f24ad15c266
Mark Chen <9006264+mark9804@users.noreply.github.com>
2026-04-01 19:29:16 +0900

    chore: 更新友链

--- docs/.vitepress/configs/navConfig.ts
+++ docs/.vitepress/configs/navConfig.ts
@@ -11,11 +11,30 @@ export const navConfig = [
           { text: "Bilibili", link: "https://space.bilibili.com/1413213021" },
         ],
       },
-      { text: "Arona Bot", link: "https://doc.arona.diyigemt.com/" },
-      { text: "BA OST Index", link: "https://ba.cnfast.top/" },
       {
-        text: "Armstrong-一位视障学生的日志",
-        link: "https://armstrong.viyf.org/",
+        text: "友站（按首字母排序）",
+        items: [
+          {
+            text: "Arona Bot",
+            link: "https://doc.arona.diyigemt.com/",
+          },
+          {
+            text: "BA OST Index",
+            link: "https://ba.cnfast.top/",
+          },
+          {
+            text: "Armstrong-一位视障学生的日志",
+            link: "https://armstrong.viyf.org/",
+          },
+          {
+            text: "妄想叛逆Project",
+            link: "https://space.bilibili.com/1989131372",
+          },
+          {
+            text: "AstralSympathy",
+            link: "https://space.bilibili.com/3690992320383242",
+          },
+        ].sort((a, b) => a.text.localeCompare(b.text)),
       },
     ],
   },
